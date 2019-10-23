42aa3f31e26ec08cdc44ad9d64661dc2e62e8c5d
dommii1218 <46981604+dommii1218@users.noreply.github.com>
2019-10-23 14:20:40 -0400

    Update and rename DB LC.sql to Database Leetcode.sql

---
+++ Database Leetcode.sql
@@ -0,0 +1,21 @@
+--175. Combine Two Tables
+--LEFT JOIN
+
+--176. Second Highest Salary
+
+--181. Employees Earning More Than Their Managers
+--SELF JOIN
+SELECT Employee
+FROM (SELECT a.Name AS Employee, a.Salary AS EmployeeSalary, b.Salary AS ManagerSalary 
+      FROM Employee a, Employee b 
+      WHERE a.ManagerId = b.Id) AS T
+WHERE EmployeeSalary > ManagerSalary
+
+--182. Duplicate Emails
+--conditions: GROUP BY ... HAVING ...
+SELECT Email
+FROM Person
+GROUP BY Email
+HAVING COUNT(*) > 1;
+
+--196. \br{Delete} Duplicate Emails
