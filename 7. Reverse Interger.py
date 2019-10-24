b40bcdbcce860206fd50265ccbf2213a8ca94fc6
dommii1218 <46981604+dommii1218@users.noreply.github.com>
2019-10-24 01:03:49 -0400

    Update 7. Reverse Interger.py

--- 7. Reverse Interger.py
+++ 7. Reverse Interger.py
@@ -2,6 +2,24 @@ class Solution:
     def reverse(self, x):
         if (-2**31) <= x <= (2**31-1):
             s = [1, -1][x < 0]      #sign
-            r = int(s * int(str(s*x)[::-1]))
-            if (-2**31) <= r <= (2**31-1): return r
-            else: return 0
+            r = s * int(str(s*x)[::-1])
+            if (-2**31) <= r <= (2**31-1): 
+                return r
+            else: 
+                return 0
+            
+-----------------------------------------------------
+class Solution:
+    def reverse(self, x: int) -> int:
+        if -2**31 <= x <= 2**31 - 1:
+            sign = [-1, 1][x > 0]
+            right = 0
+            left = abs(x)
+            while left:
+                right = right*10 + left%10
+                left = left//10
+            reverse = sign*right
+            if -2**31 <= reverse <= 2**31 - 1:
+                return reverse
+            else:
+                return 0
