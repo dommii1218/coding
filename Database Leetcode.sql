901ceca4db38891c050c493b3727a8c396987df8
dommii1218 <46981604+dommii1218@users.noreply.github.com>
2019-12-14 23:46:57 -0500

    Update Database Leetcode.sql

--- Database Leetcode.sql
+++ Database Leetcode.sql
@@ -1,7 +1,6 @@
 --175. Combine Two Tables
 --LEFT JOIN
-
-*******************************************************************************************************
+************************************************************************************************************************
 --176. Second Highest Salary
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
@@ -12,14 +11,14 @@ BEGIN
       WHERE (N-1) = (SELECT COUNT(DISTINCT(e2.Salary)) FROM Employee e2 WHERE e1.Salary < e2.Salary)
   );
 END
-                     
+************************************************************************************************************************
 --178. Rank Scores
 SELECT a.Score, COUNT(b.Score) AS Rank
 FROM Scores AS a, (SELECT DISTINCT Scores FROM Scores) AS b
 WHERE a.Score <= b.Score
 GROUP BY a.Score
 ORDER BY a.Score DESC
-
+************************************************************************************************************************
 --181. Employees Earning More Than Their Managers
 --SELF JOIN
 SELECT Employee
@@ -27,12 +26,12 @@ FROM (SELECT a.Name AS Employee, a.Salary AS EmployeeSalary, b.Salary AS Manager
       FROM Employee a, Employee b 
       WHERE a.ManagerId = b.Id) AS T
 WHERE EmployeeSalary > ManagerSalary
-
+************************************************************************************************************************
 --182. Duplicate Emails
 --conditions: GROUP BY ... HAVING ...
 SELECT Email
 FROM Person
 GROUP BY Email
 HAVING COUNT(*) > 1;
-
+************************************************************************************************************************
 --196. Delete Duplicate Emails
