2cec8ea7182f14d0e5d39cd6c24c4e763be2a22a
dommii1218 <46981604+dommii1218@users.noreply.github.com>
2019-12-14 23:46:16 -0500

    Update Database Leetcode.sql

--- Database Leetcode.sql
+++ Database Leetcode.sql
@@ -1,6 +1,7 @@
 --175. Combine Two Tables
 --LEFT JOIN
 
+*******************************************************************************************************
 --176. Second Highest Salary
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
@@ -11,6 +12,13 @@ BEGIN
       WHERE (N-1) = (SELECT COUNT(DISTINCT(e2.Salary)) FROM Employee e2 WHERE e1.Salary < e2.Salary)
   );
 END
+                     
+--178. Rank Scores
+SELECT a.Score, COUNT(b.Score) AS Rank
+FROM Scores AS a, (SELECT DISTINCT Scores FROM Scores) AS b
+WHERE a.Score <= b.Score
+GROUP BY a.Score
+ORDER BY a.Score DESC
 
 --181. Employees Earning More Than Their Managers
 --SELF JOIN
