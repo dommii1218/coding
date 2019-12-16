56f08284a70a4af56f32fe262f7770f7837a8f56
dommii1218 <46981604+dommii1218@users.noreply.github.com>
2019-12-16 00:03:36 -0500

    Update Database Leetcode.sql

--- Database Leetcode.sql
+++ Database Leetcode.sql
@@ -1,5 +1,6 @@
 --175. Combine Two Tables
 --LEFT JOIN
+
 ************************************************************************************************************************
 --176. Second Highest Salary
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
@@ -11,13 +12,22 @@ BEGIN
       WHERE (N-1) = (SELECT COUNT(DISTINCT(e2.Salary)) FROM Employee e2 WHERE e1.Salary < e2.Salary)
   );
 END
+                     
 ************************************************************************************************************************
 --178. Rank Scores
 SELECT a.Score, COUNT(b.Score) AS Rank
-FROM Scores AS a, (SELECT DISTINCT Scores FROM Scores) AS b
+FROM Scores AS a, (SELECT DISTINCT Score FROM Scores) AS b
 WHERE a.Score <= b.Score
-GROUP BY a.Score
+GROUP BY a.Id, a.Score
 ORDER BY a.Score DESC
+                     
+************************************************************************************************************************
+--180. Consecutive Numbers
+SELECT l1.Num AS ConsecutiveNums
+FROM Logs l1, Logs l2, Logs l3
+WHERE l1.Id + 1 = l2.Id AND l2.Id + 1 = l3.Id
+AND l1.Num = l2.Num AND l2.Num = l3.Num   
+                     
 ************************************************************************************************************************
 --181. Employees Earning More Than Their Managers
 --SELF JOIN
@@ -26,6 +36,7 @@ FROM (SELECT a.Name AS Employee, a.Salary AS EmployeeSalary, b.Salary AS Manager
       FROM Employee a, Employee b 
       WHERE a.ManagerId = b.Id) AS T
 WHERE EmployeeSalary > ManagerSalary
+                     
 ************************************************************************************************************************
 --182. Duplicate Emails
 --conditions: GROUP BY ... HAVING ...
